31f3bea57b67dd706b9ce76fec03c7a8a6f1d02e
Matthew D. Huff <matthewhuff89@gmail.com>
2020-09-08 22:01:19 -0700

    Add indentation

--- client/src/components/StoryForm/StoryForm.tsx
+++ client/src/components/StoryForm/StoryForm.tsx
@@ -8,22 +8,22 @@ export default function StoryForm() {
     return (
         <div className='StoryForm'>
             <form>
-            <label>
-                What is your story's name?:
-                <input type='text' id='name' onChange={(e) => setName(e.target.value)}/>
-            </label>
-            <label>
-                What type of story is this?:
-                <input type='text' id='type' onChange={(e) => setType(e.target.value)}/>
-            </label>
-            <label>
-                Is your story finished?:
-                <input type='checkbox' id='isFinished' onChange={(e) => setIsFinished(e.target.checked)} />
-            </label>
-            <button onClick={(e) => { 
-                e.preventDefault();
-                console.log(name, type, isFinished);
-            }}>Add Story To My Library</button>
+                <label>
+                    What is your story's name?:
+                    <input type='text' id='name' onChange={(e) => setName(e.target.value)}/>
+                </label>
+                <label>
+                    What type of story is this?:
+                    <input type='text' id='type' onChange={(e) => setType(e.target.value)}/>
+                </label>
+                <label>
+                    Is your story finished?:
+                    <input type='checkbox' id='isFinished' onChange={(e) => setIsFinished(e.target.checked)} />
+                </label>
+                <button onClick={(e) => { 
+                    e.preventDefault();
+                    console.log(name, type, isFinished);
+                }}>Add Story To My Library</button>
             </form>
         </div>
     );
